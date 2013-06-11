9310c74228185908677ff15369e5c93d911b0460
arya-s <arya.darc@gmail.com>
2013-06-12 04:53:06 +0900

    Testing twitter stream instead.

--- ABot2.js
+++ ABot2.js
@@ -44,6 +44,7 @@ var bot = new irc.Client(
 var fetchInterval = 2500;
 var fetchTimer = null;
 var cachedVine = null;
+var stream = twit.stream('user', { with: 'user'});
 
 bot.addListener('message', function(nick, to, text, message){
 	logger.info('['+to+'] '+nick+': '+text);
@@ -53,28 +54,39 @@ bot.addListener('message', function(nick, to, text, message){
 });
 
 function checkTweet(to){
-	fetchTimer = setInterval(function(){ 
-		twit.get('statuses/user_timeline', { screen_name: 'Lngly_', count: 1, exclude_replies: true }, function(err, data){
-			if(!err){
-				if(data[0].entities.urls.length > 0){
-					var url = data[0].entities.urls[0].expanded_url;
-					if(url.indexOf('vine.co') !== -1){
-						if(cachedVine !== url){
-							bot.say(to, 'Arya uploaded a new video: '+url);
-							cachedVine = url;
-							fetchInterval = 2500;
-							clearInterval(fetchTimer);
-							console.log('Found new vine, reseting fetch interval: '+fetchInterval);
-						} else {
-							fetchInterval = Math.min(fetchInterval+5000, 300000);
-							console.log('No new tweet, increasing fetch interval: '+fetchInterval);
-							clearInterval(fetchTimer);
-						}
-					}
+	// fetchTimer = setInterval(function(){ 
+	// 	twit.get('statuses/user_timeline', { screen_name: 'Lngly_', count: 1, exclude_replies: true }, function(err, data){
+	// 		if(!err){
+	// 			if(data[0].entities.urls.length > 0){
+	// 				var url = data[0].entities.urls[0].expanded_url;
+	// 				if(url.indexOf('vine.co') !== -1){
+	// 					if(cachedVine !== url){
+	// 						bot.say(to, 'Arya uploaded a new video: '+url);
+	// 						cachedVine = url;
+	// 						fetchInterval = 2500;
+	// 						clearInterval(fetchTimer);
+	// 						console.log('Found new vine, reseting fetch interval: '+fetchInterval);
+	// 					} else {
+	// 						fetchInterval = Math.min(fetchInterval+5000, 300000);
+	// 						console.log('No new tweet, increasing fetch interval: '+fetchInterval);
+	// 						clearInterval(fetchTimer);
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	});
+	// }, fetchInterval);
+	stream.on('tweet', function (tweet) {
+		if(tweet[0].entities.urls.length > 0){
+			var url = tweet[0].entities.urls[0].expanded_url;
+			if(url.indexOf('vine.co') !== -1){
+				if(cachedVine !== url){
+					bot.say(to, 'Arya uploaded a new video: '+url);
+					cachedVine = url;
 				}
 			}
-		});
-	}, fetchInterval);
+		}
+	});
 }
 
 function parseMessage(nick, to, text, message){
