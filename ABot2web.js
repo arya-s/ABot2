91f744ccfe0d267a47a851968756a3bc42d33911
arya-s <arya.darc@gmail.com>
2013-07-02 02:27:29 +0900

    Testin web link interface

--- ABot2web.js
+++ ABot2web.js
@@ -5,13 +5,34 @@ var logger = new (winston.Logger)({
       new (winston.transports.File)({ filename: './weblog.log' })
     ]
 });
-var express = require('express');
-var app = express();
-app.get('/', function(request, response){
-	response.send('This is the webinterface for ABot2. <a href="http://www.google.com" target="_blank">test</a>');
+var mongodb = require('mongodb');
+var url = require('url');
+var connectionUri = url.parse(process.env.MONGOHQ_URL);
+var dbName = connectionUri.pathname.replace(/^\//, '');
+//Wait for the database to connect before we start the bot.
+mongodb.Db.connect(process.env.MONGOHQ_URL, function(err, db){
+	if(err){
+		logger.error(err);
+		return err;
+	}
+	var LINKS = new mongodb.Collection(db, 'links');
+	var express = require('express');
+	var app = express();
+	app.get('/', function(request, response){
+		//Quick and dirty
+		LINKS.find().toArray(function(err, data){
+			if(!err){
+				data.forEach(function(link){
+					response.send('<p><a href="'+link.url+' target="_blank">'+link.url+'</a> by '+link.sender+' at '+Date(link.sentAt)+'</p>');
+				});
+			} else {
+				logger.error('Could not retrieve links. ',err);
+			}
+		});
+	});
+	var port = process.env.PORT || 5000;
+	app.listen(port, function() {
+		logger.info('Listening on ',port);
+	});
+	var abot2 = require('./ABot2.js');
 });
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	logger.info('Listening on ',port);
-});
-var abot2 = require('./ABot2.js');
